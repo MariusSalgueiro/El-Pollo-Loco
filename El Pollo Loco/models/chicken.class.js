a69c74987e38c150c7f6981f377b42aadea2902b
MariusSalgueiro <98022401+MariusSalgueiro@users.noreply.github.com>
2022-02-02 14:06:53 +0100

    merge conflict solved

--- El Pollo Loco/models/chicken.class.js
+++ El Pollo Loco/models/chicken.class.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class Chicken extends MovableObject{
     height = 70;
     constructor(){
@@ -7,14 +7,4 @@ class Chicken extends MovableObject{
         this.x = 200 + Math.random() * 500;
         this.y = 285;
     }
-=======
-class Chicken extends MovableObject{
-    height = 70;
-    constructor(){
-        super().loadImage('./img/3.Secuencias_Enemy_básico/Versión_Gallinita (estas salen por orden de la gallina gigantona)/1.Ga_paso_derecho.png'); //super nur bei übergeordneten methoden
-
-        this.x = 200 + Math.random() * 500;
-        this.y = 285;
-    }
->>>>>>> 49516357cc9320bce66b4cace324a808206781ce
 }
